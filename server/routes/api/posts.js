7935620a04d224c51b5463f30eb6809aeb9688fe
haquocdat543 <wwwdatha543@gmail.com>
2023-11-01 15:41:23 +0000

    Second commit

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -1,11 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const mongodb = require('mongodb')
 const router = express.Router()
+const URI = "mongodb+srv://dat:1234@cluster0.bp6tuev.mongodb.net/?retryWrites=true&w=majority"
 
 // Get Posts
-router.get('/', (req, res) => {
-  res.send('Hello World!')
+router.get('/', async (req, res) => {
+	const posts = await localPostCollection();
+	res.send( await posts.find({}).toArray());
 })
+
+async function localPostCollection() {
+	const client = await mongoose.connect(URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+		})
+	return client.URI('vue-express').collection('posts');
+}
+
 // Add Posts
 
 // Update Posts
