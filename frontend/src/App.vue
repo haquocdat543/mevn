346a1fb82bab63d29899ff4734d3d8662f75ebf8
haquocdat543 <wwwdatha543@gmail.com>
2023-11-02 06:37:57 +0000

    Fix frontend 3

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img align=center alt="Vue logo" src="./assets/logo.png">
+  <div class="three">
   <HomeView />
   <TodosView />
   <AboutView />
+  </div>
 </template>
 
 <script>
@@ -21,4 +23,27 @@ export default {
 </script>
 
 <style>
+:root {
+--first-color: #010409;
+--second-color: #0d1118;
+--navbar-color: #161b22;
+--button-color: #21262d;
+--grey-color: #c9d1d9;
+--grey-two-color: #8b949e;
+--line-color: #30363d;
+--input-color: #0d1117;
+--border-color: #30363d;
+--blue-text--color: #58a6ff;
+}
+.three { 
+display: flex;
+justify-content: space-between;
+align-items: center;
+background-color: var(--first-color);
+color: white;
+height: 60px;
+padding: 2px 28px;
+position: sticky;
+top: 0;
+}
 </style>
