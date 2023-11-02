19cf221ec376830842ac6a42c79be5ba1d438d17
haquocdat543 <wwwdatha543@gmail.com>
2023-11-02 02:56:21 +0000

    fix models

--- models/Todos.js
+++ models/Todos.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const TodoSchema = new mongoose.Schema({
-	country: String,
 	receiver: String,
+	country: String,
 	year: String
 })
 module.exports = mongoose.model('todo',TodoSchema)
